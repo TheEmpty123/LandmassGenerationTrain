dee58a849d1b058e4f49452c168595a3bb66560c
The Empty <toangaming.203@gmail.com>
2024-04-15 00:54:51 +0700

    first commit, implemented noise map generator, implemented texture map generator

---
+++ Assets/Script/MapDisplay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+
+    public Renderer textureRenderer;
+
+    public void DrawTextureMap(Texture2D texture)
+    {
+        
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+}
